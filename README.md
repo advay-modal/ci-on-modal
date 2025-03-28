2969272f93946fda648a2dad1364564b98b6b751
Advay Pal <advay@modal.com>
2025-03-28 00:59:19 -0700

    Update README and add modal server code

--- README.md
+++ README.md
@@ -1,72 +1,15 @@
-# Run Continuous Integration (CI) Tests on Modal
+# Run Continuous Integration (CI) Tests on Modal using Self hosted runners
 
-[This example repo](https://github.com/modal-labs/ci-on-modal)
+[This example repo](https://github.com/advay-modal/ci-on-modal)
 is a demonstration of one pattern for running tests on Modal:
-bring your existing package and test suite (here `my_pkg` and `tests`)
-and add a Modal App (`my_pkg.ci`)
-that mounts your tests
-and a Function (`pytest`) runs `pytest`.
+bring your existing package and test suite (here `my_pkg` and `test_lib`)
+and run it using modal sandboxes as self hosted runners
 
-That's as straightforward as
+To do this 
 
-```python
-# my_pkg/ci.py
-tests = modal.Mount.from_local_dir("path/to/tests", remote_path="/root/tests")
-
-
-@app.function(gpu="any", mounts=[tests])
-def pytest():
-    import subprocess
-
-    subprocess.run(["pytest", "-vs"], check=True, cwd="/root")
-```
-
-## Setup
-
-- Create a Python virtual environment
+- Create a personal access token in github
+- Set the personal access token in a modal [https://modal.com/docs/guide/secrets#secrets](secret) named `github-secret`
 - `pip install modal`
-- That's it 😎
+- `modal deploy modal_server.py`
 
-## Usage
-
-All commands below are run from the root of the repository.
-
-### Run tests remotely on Modal
-
-```bash
-modal run my_pkg.ci
-```
-
-On the first execution, the [container image](https://modal.com/docs/guide/custom-container)
-for your application will be built.
-
-This image will be cached on Modal and only rebuilt if one of its dependencies,
-like the `requirements.txt` file, changes.
-
-### Run tests on Modal from GitHub Actions
-
-The same command can be executed from inside a CI runner on another platform.
-We provide a sample GitHub Actions workflow in `.github/workflows/ci.yml`.
-
-To run these tests on GitHub Actions, fork this repo and
-[create a new GitHub Actions secret](https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions)
-that contains your `MODAL_TOKEN_ID` and `MODAL_TOKEN_SECRET`.
-You can find this info in the `.modal.toml` file in your home directory.
-
-Now you can [manually trigger the tests to run on GitHub Actions](https://docs.github.com/en/actions/using-workflows/manually-running-a-workflow)
-or trigger them by making a change on our fork and pushing to `main` or making a pull request.
-
-### Debug tests running remotely
-
-To debug the tests, you can open a shell
-in the exact same environment that the tests are run in:
-
-```bash
-modal shell my_pkg.ci
-```
-
-We used the `shell` feature heavily while developing this pattern!
-
-_Note_: On the Modal worker, the `pytest` command is run from the home directory, `/root`,
-which contains the `tests` folder, but the `modal shell` command will
-drop you at the top of the filesystem, `/`.
+Then trigger CI runs and modal will handle all the compute scaling.
---
+++ modal_server.py
@@ -0,0 +1,116 @@
+modal_server.py
+import modal
+import inspect
+
+app = modal.App("ci-server")
+
+def setup_runner():
+    github_token = ""
+
+    import requests
+    import subprocess
+    import uuid
+    import os
+
+    # Extract the GitHub Actions runner archive
+    print("Extracting GitHub Actions runner archive...")
+    subprocess.run(["tar", "xzf", "./actions-runner-linux-x64-2.323.0.tar.gz"])
+    print("Extraction completed.")
+
+    print("Making request to get registration token...")
+    # Make a request to get the registration token
+    github_access_token = os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"]
+
+    response = requests.post(
+        "https://api.github.com/repos/advay-modal/ci-on-modal/actions/runners/registration-token",
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {github_access_token}",
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+    )
+    print(f"Response status code: {response.status_code}")
+    token_data = response.json() 
+    registration_token = token_data["token"]
+    print(f"Registration token received: {registration_token[:5]}...")
+    
+    # Configure the runner with the registration token
+    config_command = [
+        "bash",
+        "./config.sh",
+        "--url", "https://github.com/advay-modal/ci-on-modal",
+        "--token", registration_token,
+        "--name", f"modal_runner_{uuid.uuid4().hex}",
+        "--ephemeral", 
+        "--unattended",
+    ]
+    
+    print("Configuring GitHub Actions runner...")
+    # Execute the configuration command
+    subprocess.run(config_command)
+    # List directory contents to verify files
+
+    print("Starting GitHub Actions runner...")
+    subprocess.run(["bash", "./run.sh"])   
+    subprocess.run(["bash", "./run-helper.sh"])
+    print("GitHub Actions runner process completed.")
+
+setup_runner_text = inspect.getsource(setup_runner)
+setup_runner_command = f"""{setup_runner_text}\n\nsetup_runner()"""    
+
+
+sandbox_image = modal.Image.debian_slim(python_version="3.11").apt_install([
+    "tar", "curl", "libc6", "libicu-dev", "libc-bin"
+    ]).pip_install(["requests"]).env({
+        "RUNNER_ALLOW_RUNASROOT": "1"
+    }).run_commands([
+        "curl -o actions-runner-linux-x64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz",
+    ])
+
+
+class_image = modal.Image.debian_slim().pip_install("fastapi[standard]")
+
+with class_image.imports():
+    from fastapi import Request
+
+@app.cls(allow_concurrent_inputs=1000, image = class_image)
+class ModalServer:
+
+    def __init__(self):
+        self.job_id_to_sandbox_id = {}
+
+    @modal.fastapi_endpoint(method="POST")
+    async def handle_web_request(self, request: Request):
+        # Parse the request body to get the JSON data
+        request_data = await request.json()
+        
+        if request_data["action"] == "queued":
+            sb = modal.Sandbox.create(app=app, image = sandbox_image, secrets = [modal.Secret.from_name("github-secret")])
+            p = sb.exec("python", "-c", setup_runner_command)
+            self.job_id_to_sandbox_id[request_data["workflow_job"]["id"]] = sb.object_id
+            # Read both stdout and stderr
+            for line in p.stdout:
+                # Avoid double newlines by using end="".
+                print(line, end="")
+            
+            for line in p.stderr:
+                # Print stderr with an indicator
+                print(f"ERROR: {line}", end="")
+
+        elif request_data["action"] == "completed":
+            object_id = self.job_id_to_sandbox_id.get(request_data["workflow_job"]["id"], None)
+            if object_id:
+                sb = modal.Sandbox.from_id(object_id)
+                sb.terminate()
+                del self.job_id_to_sandbox_id[request_data["workflow_job"]["id"]]
+
+            
+    
+
+
+
+
+
+
+
+# 
